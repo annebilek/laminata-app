c5ce3441b20b4cc8eb1ca430d45a84d39581161b
Anne Bilek <anne.bilek@auror.co>
2025-07-18 01:39:55 +1200

    Updates styles

--- app.js
+++ app.js
@@ -6,15 +6,18 @@ main().catch(error => {
 async function main() {
   console.log('Application started');
 
-  const productsParent = document.getElementById('products');
+  await displayProducts();
+}
+
+async function displayProducts() {
   const products = await loadData('products.json');
   const parts = await loadData('parts.json');
+  const productsParent = document.getElementById('products');
 
   products.forEach(product => {
-    console.log(`ID: ${product.id}, Name: ${product.name}`);
-
     const elem = document.createElement('li');
     elem.id = `product-${product.id}`;
+    elem.classList.add('mb-4', 'p-2', 'border', 'border-gray-300', 'rounded', 'text-lg');
     elem.textContent = product.name;
 
     const childUl = document.createElement('ul');
@@ -23,10 +26,9 @@ async function main() {
       const childButton = document.createElement('button');
       childButton.textContent = `${option.height}mm`;
       childButton.onclick = () => {
-        console.log(`Option clicked: ${option}`);
         showParts(parts.filter(part => option.parts.includes(part.id)));
       };
-      childButton.classList.add('border-blue-500', 'bg-blue-500', 'text-white', 'm-2', 'px-2', 'py-1', 'rounded');
+      childButton.classList.add('border-blue-900', 'bg-blue-600', 'text-white', 'my-1', 'px-2', 'py-1', 'rounded');
       childLi.appendChild(childButton);
       childUl.appendChild(childLi);
     });
@@ -34,28 +36,22 @@ async function main() {
 
     productsParent.appendChild(elem);
   });
-
-  
 }
 
 function showParts(filteredParts) {
   const partsList = document.getElementById('parts-list');
   partsList.classList.remove('hidden');
-  console.log('Parts list shown');
 
   const partsParent = document.getElementById('parts');
   partsParent.replaceChildren(); // Remove the first child if it exists
 
   filteredParts.forEach(part => {
-    console.log(`Part ID: ${part.id}, Name: ${part.name}`);
-
     const elem = document.createElement('li');
     elem.id = `part-${part.sku}`;
     elem.textContent = part.name;
 
     partsParent.appendChild(elem);
   });
-
 }
 
 function loadData(file) {
@@ -75,3 +71,33 @@ function loadData(file) {
       console.error('There was a problem with the fetch operation:', error);
     });
 }
+
+function validateInput() {
+  const errorMessage = document.getElementById('fence-length-error-message');
+  const input = document.getElementById('fence-length');
+  const value = input.value.trim();
+
+  if (value.length === 0 || isNaN(value)) {
+    errorMessage.textContent = 'Please enter a number';
+    return false;
+  }
+
+  if (parseFloat(value) < 2) {
+    errorMessage.textContent = 'Length must be at least 2 meters';
+    return false;
+  }
+
+  return true;
+} 
+
+const fenceLengthInput = document.getElementById('fence-length');
+fenceLengthInput.addEventListener('input', function() {
+  const errorMessage = document.getElementById('fence-length-error-message');
+  if (validateInput()) {
+    fenceLengthInput.classList.remove('border-red-500');
+    errorMessage.classList.add('hidden');
+  } else {
+    fenceLengthInput.classList.add('border-red-500');
+    errorMessage.classList.remove('hidden');
+  }
+});
